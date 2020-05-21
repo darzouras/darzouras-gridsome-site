f17adb183327d9879b87c068f315dbd22bf2b743
Darlene Zouras <dzouras@gmail.com>
2020-05-20 19:11:19 -0500

    Added scss support

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <transition name="fade">
+        <router-view />
+    </transition>
+</template>
+
+<static-query>
+    query {
+        metadata {
+            siteName
+            siteDescription
+        }
+    }
+</static-query>
+
+<script>
+export default {
+    metaInfo() {
+        return {
+            title: this.$static.metadata.siteName,
+            meta: [
+                {
+                    key: 'description',
+                    name: 'description',
+                    content: this.$static.metadata.siteDescription
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .fade-enter-active {
+        transition-duration: 0.3s;
+        transition-property: opacity;
+        transition-timing-function: ease;
+    }
+    .fade-enter,
+    .fade-leave-active {
+        opacity: 0;
+    }
+</style>
