51ae98dd788b9b82f5e7bb86322e5b1e2b8144dc
Darlene Zouras <dzouras@gmail.com>
2020-05-20 19:41:16 -0500

    updated to load all base scss automatically

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,6 +4,18 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const path = require('path')
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/styles/*.scss'),
+      ],
+    })
+}
+
 module.exports = {
   siteName: 'DarZouras.com',
   transformers: {
@@ -33,5 +45,14 @@ module.exports = {
         publicPath: '/admin',
       }
     },
-  ]
+  ],
+
+  chainWebpack(config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  }
 }
