888ff1f5e5d8cc4a5ada6a1ef518c1caffa93372
Darlene Zouras <dzouras@gmail.com>
2020-05-22 14:48:29 -0500

    updated so Blog contains query of all posts

---
+++ src/components/postlist.vue
@@ -0,0 +1,17 @@
+<template>
+    <article>
+        <h2 v-html="post.title"></h2>
+        <time datetime="post.date">
+            {{ post.date }}
+        </time>
+
+        <p v-html="post.summary"></p>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'PostList',
+    props: ['post']
+}
+</script>
---
+++ src/pages/Blog.vue
@@ -0,0 +1,37 @@
+<template>
+    <Layout>
+        <h1>Blog</h1>
+        <p>idk the description goes here</p>
+
+        <PostList v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node" />
+    </Layout>
+</template>
+
+<page-query>
+    query {
+        posts: allPost {
+            edges {
+                node {
+                    id
+                    title
+                    date
+                    summary
+                }
+            }
+        }
+    }
+</page-query>
+
+<script>
+import PostList from '~/components/postlist.vue'
+
+export default {
+    components: {
+        PostList
+    },
+    metaInfo: {
+        title: 'Blog'
+    }
+}
+</script>
+
