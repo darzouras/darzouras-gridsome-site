60220f66849af2181880c9b38b408b8832f01d46
Darlene Zouras <dzouras@gmail.com>
2020-05-31 07:55:51 -0500

    added 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,32 @@
+<template>
+    <Layout>
+        <h1>404</h1>
+
+        <p>This page does not exist.</p>
+    </Layout>
+</template>
+
+<script>
+export default {
+    metaInfo() {
+        return {
+            title: '404',
+            meta: [
+                {
+                    key: 'og:image',
+                    name: 'og:image',
+                    content: '/uploads/iStock-1142986365.jpg'
+                }
+            ]
+        }
+    },
+    methods: {
+        setHeader() {
+            this.$store.dispatch('setHeader', '/uploads/iStock-1142986365.jpg')
+        }
+    },
+    created() {
+        this.setHeader()
+    }
+}
+</script>
